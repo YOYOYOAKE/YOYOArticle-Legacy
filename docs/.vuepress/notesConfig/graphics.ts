c34023fb6ea8f25dd1dd3678f594b9f694b7c0e2
YOAKE <yo2yoake@outlook.com>
2025-03-14 16:04:26 +0800

    remove(notes/graphics): remove old notes

--- docs/.vuepress/notesConfig/graphics.ts
+++ docs/.vuepress/notesConfig/graphics.ts
@@ -6,31 +6,10 @@ export default defineNoteConfig({
   sidebar: [
     'README.md',
     {
-      text: '笔记',
       items: [
         'Part-1.md',
         'Part-2.md',
       ]
     }, 
-    {
-      text: '课堂笔记',
-      items: [
-        'Lecture-01.md',
-        'Lecture-02.md',
-        'Lecture-03.md',
-        'Lecture-04.md',
-        'Lecture-05.md',
-        'Lecture-06.md',
-        'Lecture-07.md',
-        'Lecture-08.md',
-      ]
-    },
-    {
-      text: '课后作业',
-      items: [
-        'Assignment-0.md',
-        'Assignment-1.md',
-      ]
-    },
   ]
 })
