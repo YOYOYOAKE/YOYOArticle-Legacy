0d234fd2eb10030f2576acdc01b147b0d78fe970
YOAKE <yo2yoake@outlook.com>
2025-03-21 19:06:19 +0800

    fix(record): modify structure

--- docs/.vuepress/notesConfig/record.ts
+++ docs/.vuepress/notesConfig/record.ts
@@ -4,25 +4,30 @@ export default defineNoteConfig({
   dir: 'record',
   link: '/record/',
   sidebar: [
-    'README.md',
     {
-      text: 'Archive One：初试',
+      text: '行迹 2025',
+      link: 'README.md',
       items: [
-        '2025W1.md',
-        '2025W2.md',
-        '2025W3.md',
-        '2025W4-6.md',
-        '2025W7.md',
-        '2025W8.md'
+        {
+          text: 'Archive One：初试',
+          items: [
+            '2025W1.md',
+            '2025W2.md',
+            '2025W3.md',
+            '2025W4-6.md',
+            '2025W7.md',
+            '2025W8.md'
+          ]
+        },
+        {
+          text: 'Archive Two：求索',
+          items: [
+            '2025W9.md',
+            '2025W10.md',
+            '2025W11.md',
+          ]
+        }
       ]
     },
-    {
-      text: 'Archive Two：求索',
-      items: [
-        '2025W9.md',
-        '2025W10.md',
-        '2025W11.md',
-      ]
-    }
   ]
 })
