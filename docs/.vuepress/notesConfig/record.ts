9c4d1f688b008f4ca6e8fe41f68fa49dc5cc41d3
YOAKE <yo2yoake@outlook.com>
2025-04-20 14:02:38 +0800

    add(record): 2025W15, W16

--- docs/.vuepress/notesConfig/record.ts
+++ docs/.vuepress/notesConfig/record.ts
@@ -28,6 +28,8 @@ export default defineNoteConfig({
             '2025W12.md',
             '2025W13.md',
             '2025W14.md',
+            '2025W15.md',
+            '2025W16.md',
           ]
         }
       ]
